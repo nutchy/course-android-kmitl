5253cb22ecee42ddccc32f42c96536565e8a8764
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-08-18 15:37:30 +0700

    Lab 02

---
+++ lab02/app/src/main/java/lab/first/lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package lab.first.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout_lab_03);
+//        setContentView(R.layout.activity_linearlayout_lab_01);
+    }
+}
