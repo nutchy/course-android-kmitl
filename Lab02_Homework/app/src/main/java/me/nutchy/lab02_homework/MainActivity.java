ea6167849a3876654b38abf8038f0420fb4ffc93
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-08-23 20:56:26 +0700

    Homework Lab 2

---
+++ Lab02_Homework/app/src/main/java/me/nutchy/lab02_homework/MainActivity.java
@@ -0,0 +1,13 @@
+package me.nutchy.lab02_homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flipboard);
+    }
+}
