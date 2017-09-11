cb8c2a4037ffb5c826fb5b1c5773128b069ec2a8
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-09-12 02:21:12 +0700

    Uoload missing file

---
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab04/chayanon58070021/simplemydot/model/Dots.java
@@ -0,0 +1,55 @@
+package kmitl.lab04.chayanon58070021.simplemydot.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nutchy on 9/11/2017 AD.
+ */
+
+public class Dots {
+    private List<Dot> dots = new ArrayList<>();
+
+    public List<Dot> getDots() {
+        return dots;
+    }
+
+
+
+    private OnDotsChangedListener listener;
+
+    public interface OnDotsChangedListener {
+        void onDotsChanged(Dots dots);
+    }
+
+    public void setListerner(OnDotsChangedListener listener) {
+        this.listener = listener;
+    }
+
+    public void addDot(Dot dot) {
+        this.dots.add(dot);
+        this.listener.onDotsChanged(this);
+
+    }
+
+    public void clear(){
+        this.dots.clear();
+        this.listener.onDotsChanged(this);
+    }
+
+    public int findDot(int x, int y){
+        for(int i = 0; i< dots.size(); i++){
+            double distance  = Math.sqrt(Math.pow(dots.get(i).getCenterX()-x, 2))
+                    + Math.sqrt(Math.pow(dots.get(i).getCenterY()-y, 2));
+            if (distance <= dots.get(i).getRadius()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int index){
+        this.dots.remove(index);
+        this.listener.onDotsChanged(this);
+    }
+}
---
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab04/chayanon58070021/simplemydot/view/DotView.java
@@ -0,0 +1,75 @@
+package kmitl.lab04.chayanon58070021.simplemydot.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import kmitl.lab04.chayanon58070021.simplemydot.model.Dot;
+import kmitl.lab04.chayanon58070021.simplemydot.model.Dots;
+
+
+public class DotView extends View {
+    private Paint paint;
+    private Dots dots;
+
+    public interface OnDotViewPressListener{
+        void onDotViewPressed(int x, int y);
+    }
+
+    private OnDotViewPressListener onDotViewPressListener;
+    public void setOnDotViewPressListener(
+            OnDotViewPressListener onDotViewPressListener) {
+        this.onDotViewPressListener = onDotViewPressListener;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                this.onDotViewPressListener
+                        .onDotViewPressed(
+                                (int)event.getX(),
+                                (int)event.getY());
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (dots != null) {
+            for (Dot d : dots.getDots()) {
+                paint.setColor(d.getColor());
+                canvas.drawCircle(d.getCenterX(), d.getCenterY(), d.getRadius(), paint);
+
+            }
+        }
+    }
+
+    public DotView(Context context) {
+        super(context);
+        paint = new Paint();
+    }
+
+    public DotView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+    }
+
+    public DotView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        paint = new Paint();
+    }
+
+
+    public void setDot(Dots dots) {
+        this.dots = dots;
+    }
+
+
+}
