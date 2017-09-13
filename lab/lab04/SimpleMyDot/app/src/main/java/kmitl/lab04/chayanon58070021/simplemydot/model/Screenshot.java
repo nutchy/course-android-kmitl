fc9541d528747c31fd18fdd8c161fabec6f94508
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-09-13 18:39:31 +0700

    Screenshot.java

---
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab04/chayanon58070021/simplemydot/model/Screenshot.java
@@ -0,0 +1,39 @@
+package kmitl.lab04.chayanon58070021.simplemydot.model;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created by nutchy on 9/12/2017 AD.
+ */
+
+public class Screenshot {
+
+    public static Bitmap getScreenShot(View view) {
+        View screenView = view.getRootView();
+        screenView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+        screenView.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    public File saveBitmap(Bitmap bm) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File filedir = new File(dirPath);
+        File imageFile = new File(filedir, "simplemydot.jpg");
+        FileOutputStream fOut;
+        try {
+            fOut = new FileOutputStream(imageFile);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return imageFile;
+    }
+}
