c5e49b36f9bdebd48825b91de52de47d3467f90f
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-10-13 21:44:54 +0700

    Init MyLazyInstagram

---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/kmitl/lab07/chayanon58070021/mylazyinstagram/Holder/PostItemHolder.java
@@ -0,0 +1,25 @@
+package kmitl.lab07.chayanon58070021.mylazyinstagram.Holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import kmitl.lab07.chayanon58070021.mylazyinstagram.R;
+
+/**
+ * Created by nutchy on 10/11/2017 AD.
+ */
+
+public class PostItemHolder extends RecyclerView.ViewHolder {
+    public ImageView imageView;
+    public TextView like;
+    public TextView comment;
+    public static final int ID = 1;
+    public PostItemHolder(View itemView) {
+        super(itemView);
+        imageView = (ImageView) itemView.findViewById(R.id.imageView);
+
+
+    }
+}
---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/kmitl/lab07/chayanon58070021/mylazyinstagram/Model/UserProfile.java
@@ -0,0 +1,82 @@
+package kmitl.lab07.chayanon58070021.mylazyinstagram.Model;
+
+import java.util.List;
+
+/**
+ * Created by nutchy on 10/6/2017 AD.
+ */
+
+public class UserProfile {
+    private String user;
+    private String bio;
+    private String urlProfile;
+    private int follower;
+    private int following;
+    private boolean isFollow;
+    private int post;
+    private List<Post> posts;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public int getFollower() {
+        return follower;
+    }
+
+    public void setFollower(int follower) {
+        this.follower = follower;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(int following) {
+        this.following = following;
+    }
+
+    public boolean isFollow() {
+        return isFollow;
+    }
+
+    public void setFollow(boolean follow) {
+        isFollow = follow;
+    }
+
+    public int getPost() {
+        return post;
+    }
+
+    public void setPost(int post) {
+        this.post = post;
+    }
+
+    public String getUrlProfile() {
+        return urlProfile;
+    }
+
+    public void setUrlProfile(String urlProfile) {
+        this.urlProfile = urlProfile;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
