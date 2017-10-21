c1c3c7d092a1e2a96d9a1b9d10ab96211229b70c
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-10-21 19:40:21 +0700

    Init Espresso

---
+++ lab/lab08/Espresso/app/src/main/java/lab08/kmitl/chayanon58070021/espresso/MainActivity.java
@@ -0,0 +1,13 @@
+package lab08.kmitl.chayanon58070021.espresso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
